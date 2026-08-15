0f4c2df82d0c53003ae3fcc7ea560b4238d31bc1
rpg-dev <rpg@localhost>
2026-08-14 20:26:08 -0400

    feat: add boss rematch prompt overlay screen

---
+++ js/screens/bossPromptScreen.js
@@ -0,0 +1,24 @@
+let rootEl = null;
+let callbacks = null;
+
+function render(text) {
+  rootEl.innerHTML = `
+    <div class="overlay-panel boss-prompt-panel">
+      <h2>The Dragon Returns</h2>
+      <p>${text}</p>
+      <button id="btn-boss-fight">Fight!</button>
+      <button id="btn-boss-not-yet">Not yet</button>
+    </div>
+  `;
+
+  document.getElementById('btn-boss-fight').onclick = () => callbacks.onAccept();
+  document.getElementById('btn-boss-not-yet').onclick = () => callbacks.onDecline();
+}
+
+export function mount(root, props) {
+  rootEl = root;
+  callbacks = props.callbacks;
+  render(props.text);
+}
+
+export function unmount() {}
