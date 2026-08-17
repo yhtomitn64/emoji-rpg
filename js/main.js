a1501c3533685e161d65d91c77c1fc5242ac5d05
rpg-dev <rpg@localhost>
2026-08-17 07:03:33 -0400

    fix: remove unused isWalkableAt import left over from the review fix wave

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ import { showFlavorBanner } from './screens/flavorBanner.js';
 import { applyXp, LATE_GAME_LEVEL_THRESHOLD, LEVEL_UP_PARTIAL_HEAL_FRACTION } from './systems/leveling.js';
 import { rollDrop } from './systems/loot.js';
 import { addGold, addItem, getEquipmentBonuses } from './systems/inventory.js';
-import { computeEdgeLandingPosition, isWalkableAt, isValidSavedPosition } from './systems/world.js';
+import { computeEdgeLandingPosition, isValidSavedPosition } from './systems/world.js';
 import { getMiniDungeonEntrance, isTreasureTaken, markTreasureTaken, rollMiniDungeonTreasure } from './systems/miniDungeons.js';
 import { getBossTierStats, pickBossReturnFlavor, shouldPromptForRematch, resolveBattleXp } from './systems/bossTiers.js';
 import * as bossPromptScreen from './screens/bossPromptScreen.js';
